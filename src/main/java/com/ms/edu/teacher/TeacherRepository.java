8d2fd7e685428fc2661540dc9cc62102bdfbf0bb
SCHMRC <schiavo.marco05@gmail.com>
2022-04-07 18:42:12 +0200

    add more info teacher and bug fix

--- src/main/java/com/ms/edu/teacher/TeacherRepository.java
+++ src/main/java/com/ms/edu/teacher/TeacherRepository.java
@@ -18,9 +18,9 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long>  {
 	List<Teacher> findByName(String name);
 	
 	@Modifying
-	@Query(value =  "INSERT INTO TEACHER ( NAME , SURNAME ) VALUES (:name, :surname)",nativeQuery = true)
+	@Query(value =  "INSERT INTO TEACHER ( NAME , SURNAME, ADRESS, EDUMAIL, EMAIL, PHONE ) VALUES (:name, :surname, :adress, :edumail, :email, :phone)",nativeQuery = true)
     @Transactional
-	void saveTeacher(@Param("name") String name, @Param("surname") String surname);
+	void saveTeacher(@Param("name") String name, @Param("surname") String surname,@Param("adress") String adress, @Param("edumail") String edumail,@Param("email") String email, @Param("phone") String phone);
 	
 	
 	@Query(value =  "SELECT * FROM TEACHER WHERE name = :name AND surname = :surname",nativeQuery = true)
@@ -31,7 +31,7 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long>  {
     @Transactional
     List<ArrayList<Object>> getAllTeachers();
 	
-	@Query(value =  "SELECT T.ID,  T.NAME, T.SURNAME, M.MATTER, CT.YEAR_ID, CT.SECTION_ID "
+	@Query(value =  "SELECT T.ID,  T.NAME, T.SURNAME, T.ADRESS, T.EDUMAIL, T.EMAIL, T.PHONE, M.MATTER, CT.YEAR_ID, CT.SECTION_ID "
 			+ "FROM TEACHER AS T , MATTER AS M "
 			+ "INNER JOIN MATTER_TEACH AS MT ON MT.TEACHER_ID = T.ID "
 			+ "INNER JOIN CLASS_TEACH AS CT ON CT.TEACHER_ID = T.ID "
@@ -39,6 +39,15 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long>  {
 			+ "WHERE T.ID = :id",nativeQuery = true)
     @Transactional
     List<ArrayList<Object>>  getInfoTeacher(@Param("id") Long id);
+	
+	
+	@Query(value =  "SELECT T.ID,  T.NAME, T.SURNAME, T.ADRESS, T.EDUMAIL, T.EMAIL, T.PHONE, M.MATTER "
+			+ "FROM TEACHER AS T , MATTER AS M "
+			+ "INNER JOIN MATTER_TEACH AS MT ON MT.TEACHER_ID = T.ID "
+			+ "AND MT.MATTER_ID = M.MATTER "
+			+ "WHERE T.ID = :id",nativeQuery = true)
+    @Transactional
+    List<ArrayList<Object>>  getInfoTeacherNullClass(@Param("id") Long id);
 	
 	@Query(value =  "SELECT T.ID,  T.NAME, T.SURNAME, M.MATTER, CT.YEAR_ID, CT.SECTION_ID "
 			+ "FROM TEACHER AS T , MATTER AS M "
